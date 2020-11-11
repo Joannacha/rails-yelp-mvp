955c227c175b9810e6bc628a6c7f9f2be3f06c5d
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-11 14:09:00 +0100

    added everything

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning DB"
+Restaurant.destroy_all
+
+puts"Creating 10 restaurants..."
+10.times do 
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: Restaurant::CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.cell_phone,
+  )
+  restaurant.save!
+  rand(3..10).times do
+    review = Review.new(
+      content: Faker::Restaurant.review,
+      rating: rand(1..5)
+    )
+    review.restaurant = restaurant
+    review.save!
+  end  
+end
+puts "All done"
