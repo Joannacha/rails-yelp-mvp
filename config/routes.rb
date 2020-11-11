8ac05aa61b52de11891c5a7c580bbac2ec11a87e
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-11 14:29:54 +0100

    embedded reviews/new in show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # A visitor can see the list of all restaurants.
   resources :restaurants, only: [ :index, :new, :create, :show ] do
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: [ :create ]
   end
 end
